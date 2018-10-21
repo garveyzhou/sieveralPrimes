77a3dc1c59037c25fbaa899b9a334f6ef5544135
unknown <gzhou2@stuy.edu>
2018-10-21 16:50:12 -0400

    testing sieve

--- sieve.c
+++ sieve.c
@@ -9,25 +9,27 @@ int sieve(int target){
   }
   int counter = 1;
   int i = 3;
-  int length =(int)target*log(target)*1.15;
-  int *arr = calloc(sizeOf(int),length);
+  int length =(int)(target*log(target)*1.25);
+  int *arr = calloc(sizeof(int),length);
   while(counter < target){
     if (arr[i] == 0){
-      int x = i ;
+      int x = i;
       counter ++;
       while (x < length){
 	if(x % i == 0){
 	  arr[x] = 1;
-	  x += 2;
 	}
+	x+= 2;
       }
     }
     i += 2;
   }
+  free(arr);
   return i;
 }
 
 int main(){
+  
   printf("the 25th prime number: %d \n",sieve(25));
   
   return 0;
