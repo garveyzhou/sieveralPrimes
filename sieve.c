4cc21db3f5f724f2ce645c48b8e531057911af8b
unknown <gzhou2@stuy.edu>
2018-10-21 16:54:32 -0400

    testing sieve

--- sieve.c
+++ sieve.c
@@ -11,10 +11,13 @@ int sieve(int target){
   int i = 3;
   int length =(int)(target*log(target)*1.25);
   int *arr = calloc(sizeof(int),length);
-  while(counter < target){
+  while(i < length){
     if (arr[i] == 0){
       int x = i;
       counter ++;
+      if(counter == target){
+	return i;
+      }
       while (x < length){
 	if(x % i == 0){
 	  arr[x] = 1;
@@ -25,7 +28,6 @@ int sieve(int target){
     i += 2;
   }
   free(arr);
-  return i;
 }
 
 int main(){
